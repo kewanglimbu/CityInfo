b5703c19cc0b8c893c45704e6fa03114e80a0b46
kewanglimbu <kewang.h.limbu@gmail.com>
2024-08-18 22:46:00 +0545

    Add FileController to support downloading file

---
+++ CityInfo.API/Controllers/FileController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/files")]
+    public class FileController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FileController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider;
+        }
+
+        [HttpGet("{fileId}")]
+        public ActionResult DownloadFile(string fileId)
+        {
+            var filePath = Path.Combine("BooksDownload", "Enterprise-Application-Patterns-Using-.NET-MAUI.pdf");
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+
+            if (!_fileExtensionContentTypeProvider.TryGetContentType(filePath, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+            var bytes = System.IO.File.ReadAllBytes(filePath);
+            return File(bytes, contentType, Path.GetFileName(filePath));
+        }
+
+    }
+}
