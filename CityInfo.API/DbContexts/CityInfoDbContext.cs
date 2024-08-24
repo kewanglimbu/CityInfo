aefb2a44b62bf3acec3c39dcdaa42c9490f21bf7
kewanglimbu <kewang.h.limbu@gmail.com>
2024-08-24 23:39:13 +0545

    Seed Data to Database

--- CityInfo.API/DbContexts/CityInfoDbContext.cs
+++ CityInfo.API/DbContexts/CityInfoDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using CityInfo.API.Entities;
+using CityInfo.API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CityInfo.API.DbContexts
@@ -12,5 +13,26 @@ namespace CityInfo.API.DbContexts
 
         public DbSet<City> Cities { get; set; }
         public DbSet<PointOfInterest> PointOfInterests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>().HasData(
+                new City("Gothgaun") { Id = 1, Description = "It is small peaceful semi urban area located in the morang district." },
+                new City("Nakhipot") { Id = 2, Description = "It is the beautiful place located in the satdobato, lalitpur." },
+                new City("Itahari") { Id = 3, Description = "It is city located in the Sunsari district and important business hub of eastern Nepal." }
+                );
+
+            modelBuilder.Entity<PointOfInterest>().HasData(
+                new PointOfInterest("Purbanchal University") { Id= 1, Description = "The PU is situated here.", CityId = 1 },
+                new PointOfInterest("Panchayat School") { Id= 2, Description = "The oldest government school in the gothgaun.", CityId = 1 },
+                new PointOfInterest("Kirat Yakthung Chumlung") { Id = 3, Description = "Kirat Yakthung Chumlung is a social organization of the Limbu indigenous ethnic group of Nepal.",CityId= 2 },
+                new PointOfInterest("Ullens School") { Id = 4, Description = "Ullens School is a private school in Nepal that offers holistic education from Kindergarten to IBDP.", CityId= 2 },
+                new PointOfInterest("Bhatbhateni Store") { Id = 5, Description = "It is the biggest goods store in the Satdobato.",CityId= 2 },
+                new PointOfInterest("Gorkha Department") { Id = 6, Description = "It is the oldest department store in the Itahari.",CityId= 3},
+                new PointOfInterest("Namuna College") { Id = 7, Description = "It is a distinguished educational institution nestled in the heart of Itahari Sunsari.", CityId = 3 }
+                );
+        
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ CityInfo.API/Migrations/20240824175029_InitialDataSeed.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CityInfo.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "It is small peaceful semi urban area located in the morang district.", "Gothgaun" },
+                    { 2, "It is the beautiful place located in the satdobato, lalitpur.", "Nakhipot" },
+                    { 3, "It is city located in the Sunsari district and important business hub of eastern Nepal.", "Itahari" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PointOfInterests",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "The PU is situated here.", "Purbanchal University" },
+                    { 2, 1, "The oldest government school in the gothgaun.", "Panchayat School" },
+                    { 3, 2, "Kirat Yakthung Chumlung is a social organization of the Limbu indigenous ethnic group of Nepal.", "Kirat Yakthung Chumlung" },
+                    { 4, 2, "Ullens School is a private school in Nepal that offers holistic education from Kindergarten to IBDP.", "Ullens School" },
+                    { 5, 2, "It is the biggest goods store in the Satdobato.", "Bhatbhateni Store" },
+                    { 6, 3, "It is the oldest department store in the Itahari.", "Gorkha Department" },
+                    { 7, 3, "It is a distinguished educational institution nestled in the heart of Itahari Sunsari.", "Namuna College" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
